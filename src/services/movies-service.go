49d17166c093c3939a34d2e5ec39c8ca5f2bb59b
abuUsman_04 <i123l183l@gmail.com>
2023-07-21 15:48:19 +0500

    Added some changes

--- src/services/movies-service.go
+++ src/services/movies-service.go
@@ -14,18 +14,17 @@ func init() {
 	db = *config.GetDbObject()
 }
 
-
 func GetMovies() []models.Movie {
 	var movies []models.Movie
 
-	row,err := db.Query("SELECT * FROM movies LEFT JOIN authors where movies.id = authors.Id")
+	row, err := db.Query("SELECT * FROM movies")
 
-	if err != nil{
+	if err != nil {
 		log.Fatal("There is problem in retrieving data from DB", row)
 	}
 
 	// for row.Next() {
-			
+
 	// }
 
 	// movies = append(movies, newMovie)
